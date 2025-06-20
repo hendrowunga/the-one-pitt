ad607056cab62b7ce82e93244150293c3812d54a
hendrowunga <hendrowunga073@gmail.com>
2025-06-20 18:58:51 +0700

    movement LevyWalk modification

--- src/movement/LevyWalk.java
+++ src/movement/LevyWalk.java
@@ -1,7 +1,9 @@
 /*
- * © 2025 hendrowunga, University of Sanata Dharma
- * Created on 5/30/25
+ * @author hendrowunga, University of Sanata Dharma
+ * @version 1.0
+ * @since 5/30/25
  */
+
 package movement;
 
 import core.*;
